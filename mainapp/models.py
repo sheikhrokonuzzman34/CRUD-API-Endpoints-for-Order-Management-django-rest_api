4732e78f9f9b647b446189bb3a138831017c6590
SheikhRokon <mdrokon.u545@gmail.com>
2024-05-21 15:21:14 +0600

    models and views update

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+    
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -12,8 +16,13 @@ class Order(models.Model):
     payment_information = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.total_price
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+    def __str__(self):
+        return self.order
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from .models import Order
 from .serializers import OrderSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -15,6 +16,7 @@ def home(request):
 
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def order_list(request):
     if request.method == 'GET':
         orders = Order.objects.all()
