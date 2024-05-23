758225180b73fc4588613c77cd0f9701eb6f350e
sheikhrokonuzzman34 <mdrokon.u545@gmail.com>
2024-05-23 09:39:24 +0600

    urls and views update

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -5,8 +5,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
-    path('products/', product_list),
-    path('products/<int:pk>/',product_detail),
-    path('orders/',order_list),
-    path('orders/<int:pk>/',order_detail),
+    path('products/', ProductList.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
+    path('orders/', OrderList.as_view(), name='order-list'),
+    path('orders/<int:pk>/', OrderDetail.as_view(), name='order-detail'),
 ]
--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,85 +1,34 @@
-from rest_framework.decorators import api_view,permission_classes
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework import status
-from .models import *
-from .serializers import *
-from rest_framework.permissions import IsAuthenticated
+from .models import Product, Order
+from .serializers import ProductSerializer, OrderSerializer
 
-@api_view(['GET', 'POST'])
 
-@permission_classes([IsAuthenticated])
-def product_list(request):
-    if request.method == 'GET':
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Product views
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-@permission_classes([IsAuthenticated])
-def product_detail(request, pk):
-    try:
-        product = Product.objects.get(pk=pk)
-    except Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+# Order views
+class OrderList(generics.ListCreateAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-    if request.method == 'GET':
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
 
-    elif request.method == 'PUT' or request.method == 'PATCH':
-        serializer = ProductSerializer(product, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
-    elif request.method == 'DELETE':
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-@api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticated])
-def order_list(request):
-    if request.method == 'GET':
-        orders = Order.objects.filter(user=request.user)
-        serializer = OrderSerializer(orders, many=True)
-        return Response(serializer.data)
-    
-    elif request.method == 'POST':
-        serializer = OrderSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    
-
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-@permission_classes([IsAuthenticated])
-def order_detail(request, pk):
-    try:
-        order = Order.objects.get(pk=pk, user=request.user)
-    except Order.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = OrderSerializer(order)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT' or request.method == 'PATCH':
-        serializer = OrderSerializer(order, data=request.data, partial=(request.method == 'PATCH'))
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        order.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
